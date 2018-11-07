40acd7fe1c737283c1f83e017d22fac54a47526a
Guillermo de Jesus Lopez Navarro <ESTUDIANTES\glopez@PoolCentOS605.estudiantes.ie.tec.ac.cr>
2018-11-06 20:36:28 -0600

    fix for monitor

--- monitor.sv
+++ monitor.sv
@@ -16,13 +16,15 @@ class monitor;
 	task check();
 		int exp_addr;
 		int exp_data;
-		reg [7:0]  bl; 
+		int j;
+		reg [7:0]  bl;
 		bl = sb.bfifo.pop_front(); 
 		@ (negedge intf.sys_clk);
 
-		for(j=0; j < bl; j++) begin
-			exp_addr 		= sb.afifo.pop_front();
-			exp_data        = sb.dfifo.pop_front(); // Exptected Read Data
+		for(j=0; j < bl; j++)
+		begin
+			exp_addr	= sb.afifo.pop_front();
+			exp_data	= sb.dfifo.pop_front(); // Expected Read Data
 			
 			intf.wb_stb_i        = 1;
 			intf.wb_cyc_i        = 1;
@@ -31,11 +33,12 @@ class monitor;
 			do begin
 			 @ (posedge intf.sys_clk);
 			end while(intf.wb_ack_o == 1'b0);
-			if(exp_data != intf.wb_dat_o && exp_addr != intf.wb_addr_i) // Compare expected value from scoreboard and compare with DUT output, addresses as well
+			if(exp_data != intf.wb_dat_o && exp_addr != intf.wb_addr_i) begin // Compare expected value from scoreboard and compare with DUT output, addresses as well
 				$display(" * ERROR * Read [ value/addr ] -> [ %b,%b ] ::: SB [ value/addr ] -> [ %b,%b ] ", intf.wb_addr_i, intf.wb_dat_o, exp_data, exp_addr );
-			else
-				$display(" *SUCCESS* Read [ value/addr ] -> [ %b,%b ] ::: SB [ value/addr ] -> [ %b,%b ] ", intf.wb_addr_i, intf.wb_dat_o, exp_data, exp_addr );
 			end 
+			else begin
+				$display(" *SUCCESS* Read [ value/addr ] -> [ %b,%b ] ::: SB [ value/addr ] -> [ %b,%b ] ", intf.wb_addr_i, intf.wb_dat_o, exp_data, exp_addr );
+			end
 			@ (negedge intf.sdram_clk);
 		end
 		intf.wb_stb_i        = 0;
@@ -44,7 +47,6 @@ class monitor;
 		intf.wb_addr_i       = 'hx;
 		
 	endtask
-	
 
 endclass
 
