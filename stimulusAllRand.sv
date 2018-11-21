e376a5f4db3c21ea49357c4d8a233ac686b0eec5
Guillermo de Jesus Lopez Navarro <ESTUDIANTES\glopez@PoolCentOS604.estudiantes.ie.tec.ac.cr>
2018-11-20 20:41:49 -0600

    WIP 2 estimulos

--- stimulusAllRand.sv
+++ stimulusAllRand.sv
@@ -2,14 +2,24 @@
 `define STIMULUS_ALLRAND_SV
 
 class stimulusAllRand;
-  rand  logic[11:0] row;
-  rand  logic[1:0]  bank;
-  rand  logic[11:0] col;
-  //constraint distribution {value dist { 0  := 1 , 1 := 1 }; };
+    rand  logic[11:0] row;
+    rand  logic[1:0]  bank;
+    rand  logic[11:0] col;
 
-	// memory layout
-	// Row Address[11:0] Bank Address[1:0] Column Address[11:0]
-//  function getStimulus(int row, int bank, int col;
+    constraint memoryrange {
+        //bank dist { 0 := 1 , 1 := 1 };
+        bank >= 0; bank < 5;
+        row >= 0; row < 12;
+        col >= 0; col < 8;
+    };
+
+    // memory layout
+    // sdram controller supports the following amount of rows, banks, and columns:
+    //                              4M*16 Config ->       12     4           8
+    //                              2M*32 Config ->       11     4           8
+    
+    // Row Address[11:0] Bank Address[1:0] Column Address[11:0]
+    //  function getStimulus(int row, int bank, int col;
 
 endclass
 
