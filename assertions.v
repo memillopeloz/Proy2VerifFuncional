36d460cb514f3311c90344d98cfbd95c112b9cc8
aloloap <aloaizapereira@gmail.com>
2018-12-01 14:47:27 -0600

    Added latest version of the sdram initialization assertion

--- assertions.v
+++ assertions.v
@@ -30,8 +30,7 @@ sequence autorefresh_cmd;
 endsequence
 
 property prop_sdram_correct_init;
-	@(posedge wbox_intf.sdram_clk) $rose(wbox_intf.sdram_en) 	|=> (inhibit_cmd or nop_cmd) [*10000]
-																|=> nop_cmd[*0:$] |=> precharge_cmd |=> nop_cmd[*0:$] |=> autorefresh_cmd |=> nop_cmd[*0:$] |=> autorefresh_cmd;
+	@(posedge wbox_intf.sdram_clk) $rose(wbox_intf.sdram_en) 	|=> nop_cmd [*10000] |=> nop_cmd[*0:$] ##1 precharge_cmd[*2] ##1 nop_cmd[*0:$] ##1 autorefresh_cmd[*2] ##1 nop_cmd[*0:$] ##1 autorefresh_cmd[*2];
 endproperty;
 
 property prop_wb_sigs_init_during_rst;
